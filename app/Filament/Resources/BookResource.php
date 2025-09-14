93c2f3ce2a55ed6ce689723acf45285be1c5c7fd
osaidhaj <osaidhaj03@gmail.com>
2025-09-14 12:11:32 +0300

    refactor(tables): remove modalColumns setting from table column toggle action

--- app/Filament/Resources/BookResource.php
+++ app/Filament/Resources/BookResource.php
@@ -976,7 +976,6 @@ class BookResource extends Resource
                     ->icon('heroicon-o-view-columns')
                     ->color('gray')
                     ->tooltip('إظهار/إخفاء الأعمدة')
-                    ->modalColumns(3) // هذا يجعل الخيارات تظهر 3 في كل صف
             );
     }
 
