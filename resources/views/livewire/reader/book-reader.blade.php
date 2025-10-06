57298f5eed1b989349032231c8e76cc237455176
osaidhaj <osaidhaj03@gmail.com>
2025-10-06 23:27:01 +0300

    style(reader): increase md size of book reader icon from 8x8 to 16x16

--- resources/views/livewire/reader/book-reader.blade.php
+++ resources/views/livewire/reader/book-reader.blade.php
@@ -27,7 +27,7 @@
                     <!-- عنوان الصفحة -->
                     <div class="mb-6 sm:mb-8 md:mb-10 lg:mb-12 z-10">
                         <div class="flex flex-col sm:flex-row items-center gap-3 sm:gap-4 mb-4 sm:mb-6">
-                            <img src="{{ asset('images/group0.svg') }}" alt="Icon" class="w-6 h-6 sm:w-7 sm:h-7 md:w-8 md:h-8">
+                            <img src="{{ asset('images/group0.svg') }}" alt="Icon" class="w-6 h-6 sm:w-7 sm:h-7 md:w-16 md:h-16">
                             <div class="text-center sm:text-right">
                                 <h2 class="text-2xl sm:text-3xl md:text-4xl text-[#5D6019] font-bold font-tajawal">
                                     <a href="{{ route('books.details', $book->id) }}" 
